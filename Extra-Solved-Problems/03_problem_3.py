dbe86deb2a534f1dde276c270adfc8cb261d903f
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-22 22:02:44 +0530

    Python

---
+++ Extra-Solved-Problems/03_problem_3.py
@@ -0,0 +1,10 @@
+'''
+Student Performance Tracker
+Problem Statement:
+Write a program to manage student data and analyze their performance.
+- Use a file to store student data (name, marks in 3 subjects).
+- Read the data from the file into a dictionary where the key is the student's name and the value is a tuple of their marks.
+- Calculate and display the average marks for each student using a loop.
+- Identify students scoring above 75% and write their names to a new file.
+- Allow the user to add new student data using input and update the file accordingly.
+'''
