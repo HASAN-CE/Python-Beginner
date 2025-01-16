a71cbceba80c2acd104772a3955dd63fd9c93cba
HASAN-CE <hasanrangooni308@gmail.com>
2025-01-16 12:42:55 +0530

    Python

--- Day_02/Day-2 Practice Set Solution/04_Extra_Ques.py
+++ Day_02/Day-2 Practice Set Solution/04_Extra_Ques.py
@@ -1,4 +1,8 @@
 'Write a Python program to check the type of a variable using the `type()` function.'
 
-a = int(input('Enter the number to know its type: '))
-print(type(a))
+# Prompt the user to input a number and convert it to an integer
+# Note: This will raise a ValueError if the input is not a valid integer
+a = int(input('Enter a number to determine its type: '))
+
+# Display the type of the variable 'a' using the built-in `type()` function
+print(f"The type of the variable is: {type(a)}")
---
+++ Day_02/Day-2 Practice Set Solution/05_Extra_Ques.py
@@ -0,0 +1,13 @@
+'Assign multiple values to multiple variables in a single line and print them.'
+
+# Each variable (a, b, c, d, e) is assigned a corresponding value from the tuple (1, 2, 3, 4, 5)
+a, b, c, d, e = 1, 2, 3, 4, 5
+
+# Print the values of the variables in a formatted way
+# The print() function accepts multiple arguments, separated by commas, and adds spaces between them
+print("Values are:", a, b, c, d, e)
+
+
+
+
+
