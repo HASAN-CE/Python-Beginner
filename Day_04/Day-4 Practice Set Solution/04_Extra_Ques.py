314d2001e2a174c607fb2e96ec04c5d4a029eb95
HASAN-CE <hasanrangooni308@gmail.com>
2025-01-23 22:37:41 +0530

    'Python'

--- Day_04/Day-4 Practice Set Solution/04_Extra_Ques.py
+++ Day_04/Day-4 Practice Set Solution/04_Extra_Ques.py
@@ -1,1 +1,15 @@
 'Sort a list of tuples by the second element of each tuple' 
+
+# Define a list of tuples
+tuples_list = [(1, 3), (4, 1), (2, 5), (7, 2)]
+
+# Sort the list of tuples based on the second element of each tuple
+# The lambda function specifies that sorting should consider x[1] (second element of the tuple)
+sorted_list = sorted(tuples_list, key=lambda x: x[1])
+
+# Print the original list (unchanged)
+print("Original List:", tuples_list)
+
+# Print the sorted list (ordered by the second element)
+print("Sorted List:", sorted_list)
+
